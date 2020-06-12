881f31e75d86685e82d08de6125f8f32a8bd2fda
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-06-12 14:21:42 -0400

    Initial commit for SeamedIn Final Project

---
+++ GC_FinalProject_Seamless_June2020/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GC_FinalProject_Seamless_June2020.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ GC_FinalProject_Seamless_June2020/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GC_FinalProject_Seamless_June2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
