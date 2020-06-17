0a5a4765b38921308b7281bf6644a9bc769251bb
Josh Forner <fornerjos@gmail.com>
2020-06-17 13:12:38 -0400

    More Admin functions

--- GC_FinalProject_Seamless_June2020/Controllers/AdminController.cs
+++ GC_FinalProject_Seamless_June2020/Controllers/AdminController.cs
@@ -13,42 +13,51 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace GC_FinalProject_Seamless_June2020.Controllers
 {
-        [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly IServiceProvider serviceProvider;
         private readonly SeamedInDBContext _context;
+
         public AdminController(IServiceProvider serviceProvider, SeamedInDBContext context)
         {
             this.serviceProvider = serviceProvider;
             _context = context;
         }
-        [Authorize(Roles = "Admin")]
+
         public IActionResult Index()
         {
             var users = _context.AspNetUsers.ToList();
             return View(users);
         }
-        [Authorize(Roles = "Admin")]
+
         public async Task<IActionResult> AdminView()
         {
             var users = _context.Users.ToList();
             return View(users);
         }
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AddRole(string email, string usertype)
+
+        public async Task<IActionResult> AddRole(string id, string usertype)
         {
-            await CreateRoles(email, usertype);
+            await CreateRoles(id, usertype);
             return RedirectToAction("Index");
         }
 
-        public async Task<IActionResult> Update(string email, string newtype, string newtheme, string newname, string newtech, string newlandscape, string newindustry, string newphone,
+        public async Task<IActionResult> UpdateForm(string id)
+        {
+
+            var user = _context.Users.Where(x => x.UserId == id).First();
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> Update(string id, string newtype, string newtheme, string newname, string newtech, string newlandscape, string newindustry, string newphone,
             string newemail, string newwebsite, string newpic, string newcity, string newstate, string newcountry)
         {
-            var users = _context.Users.Where(x => x.EmailAddress == email).ToList();
-            foreach (Users user in users)
-            {
-                if (user.EmailAddress == email)
+            var user = _context.Users.Where(x => x.UserId == id).First();
+
+
+            if (ModelState.IsValid)
                 {
                     user.UserType = newtype;
                     user.Theme = newtheme;
@@ -62,55 +71,80 @@ namespace GC_FinalProject_Seamless_June2020.Controllers
                     user.City = newcity;
                     user.StateProvince = newstate;
                     user.Country = newcountry;
+                    user.ProfilePicture = newpic;
+                    await _context.SaveChangesAsync();
                 }
-            }
-            return RedirectToAction("Index");
+            
+            return RedirectToAction("AdminView");
         }
 
-        public async Task<IActionResult> Delete(string email)
+        public async Task<IActionResult> Delete(string id)
         {
-            var users =  _context.Users.Where(x => x.EmailAddress == email).ToList();
-            foreach(Users user in users)
-            {
-                if(user.EmailAddress == email)
-                {
+            var user = _context.Users.Where(x => x.UserId == id).First();
+           
                     _context.Users.Remove(user);
-                }
-            }
+                
+            
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
-        [Authorize(Roles = "Admin")]
-        private async Task CreateRoles( string email, string usertype)
+        public async Task<IActionResult> DeleteAccount(string id)
         {
-            if (email != "example@example.com")
-            {
-                var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-                var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-                //here in this line we are adding Admin Role
-                var roleCheck = await RoleManager.RoleExistsAsync(usertype);
-                if (!roleCheck)
-                {
-                    //here in this line we are creating admin role and seed it to the database
-                    await RoleManager.CreateAsync(new IdentityRole(usertype));
-                }
-                //here we are assigning the Admin role to the User that we have registered above 
-                //Now, we are assinging admin role to this user("Ali@gmail.com"). When will we run this project then it will
-                //be assigned to that user.
-                IdentityUser user = await UserManager.FindByEmailAsync(email);
-                await UserManager.AddToRoleAsync(user, usertype);
-                if (usertype == "Admin")
-                {
-                    await UserManager.RemoveFromRoleAsync(user, "Startup");
-                }
-                else if (usertype == "Startup")
-                {
-                    await UserManager.RemoveFromRoleAsync(user, "Admin");
-                    _ = await UserManager.UpdateSecurityStampAsync(user);
-                    
-                }
+            var user = _context.AspNetUsers.Where(x => x.Id == id).First();
+           
                 
+                    _context.AspNetUsers.Remove(user);
+                
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private async Task CreateRoles(string id, string usertype)
+        {
+
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            //here in this line we are adding Admin Role
+            var roleCheck = await RoleManager.RoleExistsAsync(usertype);
+            if (!roleCheck)
+            {
+                //here in this line we are creating admin role and seed it to the database
+                await RoleManager.CreateAsync(new IdentityRole(usertype));
+            }
+            //here we are assigning the Admin role to the User that we have registered above 
+            //Now, we are assinging admin role to this user("Ali@gmail.com"). When will we run this project then it will
+            //be assigned to that user.
+            IdentityUser user = await UserManager.FindByIdAsync(id);
+            await UserManager.AddToRoleAsync(user, usertype);
+            if (usertype == "Admin")
+            {
+                var user1 = _context.AspNetUsers.Find(id);
+                user1.Roles = "Admin";
+                await _context.SaveChangesAsync();
+                await UserManager.RemoveFromRoleAsync(user, "Startup");
+                await UserManager.RemoveFromRoleAsync(user, "Partner");
+            }
+            else if (usertype == "Startup")
+            {
+                var user1 = _context.AspNetUsers.Find(id);
+                user1.Roles = "Startup";
+                await _context.SaveChangesAsync();
+                await UserManager.RemoveFromRoleAsync(user, "Admin");
+                await UserManager.RemoveFromRoleAsync(user, "Partner");
+                _ = await UserManager.UpdateSecurityStampAsync(user);
+
+            }
+            else if (usertype == "Partner")
+            {
+                var user1 = _context.AspNetUsers.Find(id);
+                user1.Roles = "Partner";
+                await _context.SaveChangesAsync();
+                await UserManager.RemoveFromRoleAsync(user, "Admin");
+                await UserManager.RemoveFromRoleAsync(user, "Startup");
+                _ = await UserManager.UpdateSecurityStampAsync(user);
+
             }
         }
 
